0aa437a834d6e700120b5cfb8687364eec976f29
christiancajiao <52766780+christiancajiao@users.noreply.github.com>
2019-07-25 23:34:00 -0500

    pagina funcional pero no practica
    el codigo javascript funciona bien, aun hay que agregar mas funciones, como reinicio;  estilizar mas la pagina.

---
+++ app.js
@@ -0,0 +1,22 @@
+var colores = document.getElementsByClassName("color");
+var pixeles = document.getElementsByClassName("pixel");
+var colorSeleccionado;
+
+//for para recorrer la paleta de colores
+for(var i = 0; i < colores.length; i++) {
+    colores[i].addEventListener("click", colorPick);
+}
+//funcion para obtener el elemento en el que estoy haciendo click, con estilos
+function colorPick (e) {
+    colorSeleccionado = window.getComputedStyle(e.target).backgroundColor;
+   
+}
+//for para recorrer todos los pixeles
+for(var j = 0; j < pixeles.length; j++) {
+    pixeles[j].addEventListener("click", paintool)
+}
+// funcion para agregar un estilo al html en este caso el color de fondo
+function paintool(e) {
+    e.target.style.backgroundColor = colorSeleccionado;
+    console.log(colorSeleccionado);
+}
